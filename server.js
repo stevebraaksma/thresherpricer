490b828a0ad2927960496e14d6eee6dc6f6c3b33
Steve Braaksma <stevebraaksma@hotmail.com>
2022-01-07 07:29:47 -0600

    add middleware

--- server.js
+++ server.js
@@ -3,3 +3,14 @@ const app = express();
 const axios = require('axios');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`listening on port: ${PORT}`);
+});
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.use(express.json());
