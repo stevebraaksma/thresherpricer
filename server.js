03ea4e20fa12b1b7a41a8801de9074dd5edbdf21
Steve Braaksma <stevebraaksma@hotmail.com>
2022-01-07 07:45:14 -0600

    add post route for form

--- server.js
+++ server.js
@@ -20,3 +20,16 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
+
+
+app.post('/parts', (req, res) => {
+    // res.render('index.ejs')
+    console.log('heyyyy');
+    console.log(req.body.tester)
+    res.redirect('/');
+
+    // to do:
+    // take the form data, push it to an array (maybe, an array of objects, each object having 3 fields)
+    // then, will pass this data to the mouser function, which will for each loop through, sending a pricing req for each
+
+})
