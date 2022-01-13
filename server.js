1646ffe1b984a2d6768be66139d42b8103ab3005
Steve Braaksma <stevebraaksma@hotmail.com>
2022-01-13 10:22:59 -0600

    add middleware for static public files

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.use(express.json());
+app.use(express.static('public'));
 
 let partNumArray = [];
 let partQtyArray = [];
